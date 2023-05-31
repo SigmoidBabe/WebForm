814a1df274c414486c7d34c722602753cf5dc999
SigmoidBabe <sigmoidbabe@gmail.com>
2023-05-31 11:46:21 +0700

    some change on main file

--- api/CobaWTF.py
+++ api/CobaWTF.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, redirect, url_for
-from courseform import CourseForm
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+                     RadioField)
+from wtforms.validators import InputRequired, Length
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '15c1b7efdf870d0315df78598f99ca31e83e33776af579bb'
 
 
+class CourseForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),
+                                             Length(min=10, max=100)])
+    description = TextAreaField('Course Description',
+                                validators=[InputRequired(),
+                                            Length(max=200)])
+    price = IntegerField('Price', validators=[InputRequired()])
+    level = RadioField('Level',
+                       choices=['Beginner', 'Intermediate', 'Advanced'],
+                       validators=[InputRequired()])
+    available = BooleanField('Available', default='checked')
+
 courses_list = [{
-    'title': 'Python 101',
-    'description': 'Learn Python basics',
+    'title': 'Deploy Web Flask',
+    'description': 'Belajar Mendeploy Web Flask',
     'price': 34,
     'available': True,
     'level': 'Beginner'
