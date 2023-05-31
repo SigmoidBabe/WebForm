898faf3a9af64b74319072988c06cca41833e866
SigmoidBabe <sigmoidbabe@gmail.com>
2023-05-31 10:20:14 +0700

    try to add form

--- api/CobaWTF.py
+++ api/CobaWTF.py
@@ -1,10 +1,47 @@
 from flask import Flask, render_template, redirect, url_for
-
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+                     RadioField)
+from wtforms.validators import InputRequired, Length
 
 app = Flask(__name__)
 
+class CourseForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(),
+                                             Length(min=10, max=100)])
+    description = TextAreaField('Course Description',
+                                validators=[InputRequired(),
+                                            Length(max=200)])
+    price = IntegerField('Price', validators=[InputRequired()])
+    level = RadioField('Level',
+                       choices=['Beginner', 'Intermediate', 'Advanced'],
+                       validators=[InputRequired()])
+    available = BooleanField('Available', default='checked')
 
-@app.route('/')
+courses_list = [{
+    'title': 'Python 101',
+    'description': 'Learn Python basics',
+    'price': 34,
+    'available': True,
+    'level': 'Beginner'
+    }]
+
+
+@app.route('/', methods=('GET', 'POST'))
 def page_isi_form():
-    return "BERHASIL UYY"
+    form = CourseForm()
+    if form.validate_on_submit():
+        courses_list.append({'title': form.title.data,
+                             'description': form.description.data,
+                             'price': form.price.data,
+                             'available': form.available.data,
+                             'level': form.level.data
+                             })
+        return redirect(url_for('courses'))
+    return render_template('CobaWTF_Form.html', form=form)
+
+@app.route('/courses')
+def courses():
+    return render_template('List_Course.html', courses_list=courses_list)
+
 
